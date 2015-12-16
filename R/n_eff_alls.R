ce536315f059f31325199fbe01876f91a6a17147
jmsigner <jmsigner@gmail.com>
2015-12-16 09:38:01 +0100

    upd

--- R/n_eff_alls.R
+++ R/n_eff_alls.R
@@ -4,6 +4,6 @@ n_eff_alls <- function(x, stratum) {
   if (missing(stratum)) {
     1 / (1 - het_exp(x))
   } else {
-    sapply(split(1:nrow(x), stratum), function(i, g) n_eff_alls(g[i, , ]), g = x)
+    t(sapply(split(1:nrow(x), stratum), function(i, g) n_eff_alls(g[i, , ]), g = x))
   }
 }
